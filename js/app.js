66c13abd6b5c3b470cb1cc4b3622ca8ab957508e
muhayminul <muhayminul96@gmail.com>
2022-03-11 19:50:12 +0600

    final submit logical error fixed

--- js/app.js
+++ js/app.js
@@ -40,7 +40,6 @@ const switchTab = (id) => {
     } else if (id === "liked") {
         document.getElementById( "liked" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
-        document.getElementById( "posts" ).textContent = "";
         document.getElementById( "reported" ).textContent = "";
         document.getElementById( "reported" ).style.display = "none";
 
@@ -49,7 +48,6 @@ const switchTab = (id) => {
         document.getElementById( "reported" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "liked" ).textContent = "";
-        document.getElementById( "posts" ).textContent = "";
         document.getElementById( "liked" ).style.display = "none";
 
         displayReportedPosts();
